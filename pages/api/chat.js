33985d81fe9a9a8033b3287ce185d63e88c30ecc
Olubusayo Amowe <amoweolubusayo@gmail.com>
2025-01-27 22:18:14 +0000

    integrate deepseek

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  basePath: "https://api.deepseek.com/v1",
+  apiKey: process.env.DEEP_KEY_API,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -9,15 +10,12 @@ const openai = new OpenAIApi(configuration);
 export default async function handler(req, res) {
   const imput = req.body.imput || "";
   try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      temperature: 0.6,
+    const response = await openai.createChatCompletion({
+      model: "deepseek-chat",
       messages: [{ role: "user", content: generateAffirmation(imput) }],
     });
-    const value = response.data["choices"][0]["message"]["content"].split('\n')
-    res
-      .status(200)
-      .json({ affirmation: value[0].trim().replace(/^\d+\.\s*/, '') });
+    const value = response.data["choices"][0]["message"]["content"].split("\n");
+    res.status(200).json({ affirmation: value });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred." });
